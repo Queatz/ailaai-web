d0b8037cdd797aa6b25c5fbf1053ae859b329e14
Jacob Ferrero <jacobaferrero@gmail.com>
2023-11-15 11:59:16 +0700

    CardReply component

--- src/jsMain/kotlin/components/CardPage.kt
+++ src/jsMain/kotlin/components/CardPage.kt
@@ -5,25 +5,17 @@ import androidx.compose.runtime.*
 import api
 import app.ailaai.api.card
 import app.ailaai.api.cardsCards
-import app.ailaai.api.wildReply
 import app.softwork.routingcompose.Router
 import appString
-import application
 import com.queatz.db.*
 import json
 import kotlinx.browser.window
-import kotlinx.coroutines.launch
-import notEmpty
-import org.jetbrains.compose.web.attributes.autoFocus
-import org.jetbrains.compose.web.attributes.disabled
-import org.jetbrains.compose.web.attributes.placeholder
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import r
 
 @Composable
 fun CardPage(cardId: String, onError: () -> Unit = {}, cardLoaded: (card: Card) -> Unit) {
-    val me by application.me.collectAsState()
     var isLoading by remember { mutableStateOf(false) }
     var card by remember { mutableStateOf<Card?>(null) }
     var cards by remember { mutableStateOf<List<Card>>(emptyList()) }
@@ -32,8 +24,6 @@ fun CardPage(cardId: String, onError: () -> Unit = {}, cardLoaded: (card: Card) 
     var cardOptions by remember { mutableStateOf<CardOptions?>(null) }
     var isReplying by remember { mutableStateOf<List<ConversationItem>?>(null) }
     var replyMessage by remember { mutableStateOf("") }
-    var isSendingReply by remember { mutableStateOf(false) }
-    val scope = rememberCoroutineScope()
     val router = Router.current
 
     LaunchedEffect(cardId) {
@@ -59,31 +49,6 @@ fun CardPage(cardId: String, onError: () -> Unit = {}, cardLoaded: (card: Card) 
         } finally {
             isLoading = false
         }
-    }
-
-    val sentString = appString { messageWasSent }
-    val didntWorkString = appString { didntWork }
-
-    suspend fun sendMessage() {
-        isSendingReply = true
-        val body = WildReplyBody(
-            message = replyMessage,
-            conversation = isReplying!!.map { it.title }.filter { it.isNotBlank() }
-                .notEmpty?.joinToString(" → "),
-            card = cardId,
-            device = api.device
-        )
-        api.wildReply(
-            body,
-            onError = {
-                window.alert(didntWorkString)
-            }
-        ) {
-            replyMessage = ""
-            isReplying = null
-            window.alert(sentString)
-        }
-        isSendingReply = false
     }
 
     if (!isLoading && card == null) {
@@ -162,126 +127,27 @@ fun CardPage(cardId: String, onError: () -> Unit = {}, cardLoaded: (card: Card) 
                                     LinkifyText(message)
                                 }
                             }
-                            if (isReplying != null) {
-                                val includeContactString = appString { includeContact }
-                                // todo can be EditField
-                                TextArea(replyMessage) {
-                                    classes(Styles.textarea)
-                                    style {
-                                        width(100.percent)
-                                        height(8.r)
-                                        marginBottom(1.r)
-                                    }
-
-                                    if (me == null) {
-                                        placeholder(includeContactString)
-                                    }
-
-                                    if (isSendingReply) {
-                                        disabled()
-                                    }
-
-                                    onInput {
-                                        replyMessage = it.value
-                                    }
-
-                                    autoFocus()
+                            CardReply(
+                                card,
+                                cardOptions,
+                                cardConversation,
+                                stack,
+                                replyMessage,
+                                isReplying,
+                                onCardConversation = {
+                                    cardConversation = it
+                                },
+                                onMessageSent = {
+                                    replyMessage = ""
+                                    isReplying = null
+                                },
+                                onIsReplying = {
+                                    isReplying = it
+                                },
+                                onReplyMessage = {
+                                    replyMessage = it
                                 }
-                            }
-                            if (isReplying != null) {
-                                Div({
-                                    style {
-                                        display(DisplayStyle.Flex)
-                                        marginBottom(0.r)
-                                    }
-                                }) {
-                                    Button({
-                                        classes(Styles.button)
-                                        style {
-                                            marginRight(1.r)
-                                        }
-                                        onClick {
-                                            scope.launch {
-                                                sendMessage()
-                                            }
-                                        }
-                                        if (isSendingReply || replyMessage.isBlank()) {
-                                            disabled()
-                                        }
-                                    }) {
-                                        Text(appString { sendMessage })
-                                    }
-                                    Button({
-                                        classes(Styles.outlineButton)
-                                        onClick {
-                                            isReplying = null
-                                        }
-                                        if (isSendingReply) {
-                                            disabled()
-                                        }
-                                    }) {
-                                        Text(appString { cancel })
-                                    }
-                                }
-                            } else {
-                                cardConversation?.items?.forEach { item ->
-                                    when (item.action) {
-                                        ConversationAction.Message -> {
-                                            Button({
-                                                classes(Styles.button)
-                                                onClick {
-                                                    isReplying = stack + (cardConversation?.let(::listOf)
-                                                        ?: emptyList()) + item.let(::listOf)
-                                                }
-                                            }) {
-                                                Span({
-                                                    classes("material-symbols-outlined")
-                                                }) {
-                                                    Text("message")
-                                                }
-                                                Text(" ${item.title}")
-                                            }
-                                        }
-
-                                        else -> {
-                                            Button({
-                                                classes(Styles.outlineButton)
-                                                onClick {
-                                                    stack.add(cardConversation!!)
-                                                    cardConversation = item
-                                                }
-                                            }) {
-                                                Text(item.title)
-                                            }
-                                        }
-                                    }
-                                }
-                                if (cardConversation?.items.isNullOrEmpty() && cardOptions?.enableAnonymousReplies != false) {
-                                    Button({
-                                        classes(Styles.button)
-                                        onClick {
-                                            isReplying = stack + (cardConversation?.let(::listOf) ?: emptyList())
-                                        }
-                                    }) {
-                                        Span({
-                                            classes("material-symbols-outlined")
-                                        }) {
-                                            Text("message")
-                                        }
-                                        Text(" ${appString { message }}")
-                                    }
-                                }
-                                if (stack.isNotEmpty()) {
-                                    Button({
-                                        classes(Styles.outlineButton)
-                                        onClick {
-                                            cardConversation = stack.removeLast()
-                                        }
-                                    }) {
-                                        Text(appString { goBack })
-                                    }
-                                }
-                            }
+                            )
                         }
                     }
                 }
